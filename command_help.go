f2d3571fe989b13deb311812ce3f82601ffa13e7
SGWinter <seangcalgary@gmail.com>
2025-01-06 11:57:10 -0700

    using getCommands rather than init to initialize the command list

--- command_help.go
+++ command_help.go
@@ -5,7 +5,7 @@ import "fmt"
 func commandHelp() error {
 	fmt.Printf("Welcome to the Pokedex!\n")
 	fmt.Printf("Usage:\n\n")
-	for _, c := range commandList {
+	for _, c := range getCommands() {
 		fmt.Printf("%v: %v\n", c.name, c.description)
 	}
 	return nil
--- repl.go
+++ repl.go
@@ -7,28 +7,7 @@ import (
 	"bufio"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
 
-var commandList = map[string]cliCommand{}
-
-func init() {
-	commandList = map[string]cliCommand {
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-	}
-}
 
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
@@ -43,35 +22,43 @@ func startRepl() {
 
 		commandName := inputText[0]
 
-		if commandName == commandList["exit"].name {
-			commandList["exit"].callback()
-		} else if commandName == commandList["help"].name {
-			commandList["help"].callback()
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
 		} else {
-			fmt.Printf("Unknown command\n")
+			fmt.Println("Unknown command")
+			continue
 		}
 	}
-}
-
-func commandExit() error {
-	fmt.Printf("Closing the Pokedex... Goodbye!\n")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Printf("Welcome to the Pokedex!\n")
-	fmt.Printf("Usage:\n\n")
-
-	for _, c := range commandList {
-		fmt.Printf("%v: %v\n", c.name, c.description)
-	}
-
-	return nil
 }
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand {
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
