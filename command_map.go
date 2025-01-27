72d8be12945516c1f83b94fa33f0286eb29655d3
SGWinter <seangcalgary@gmail.com>
2025-01-26 18:22:49 -0700

    combining map and mapb into same function

--- command_map.go
+++ command_map.go
@@ -8,23 +8,62 @@ import (
 	"encoding/json"
 )
 
-func commandMap(s *config) error {
-	type resultsBody struct {
-		Name string `json:"name"`
-		Url  string `json:"url"`
-	}
+type resultsBody struct {
+	Name string `json:"name"`
+	Url  string `json:"url"`
+}
 
-	type bodyResults struct {
-		Count    int    `json:"count"`
-		Next     string `json:"next"`
-		Previous string `json:"previous"`
-		Results  []resultsBody  `json:"results"`
-	}
+type bodyResults struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []resultsBody  `json:"results"`
+}
+
+func commandMap(s *config) error {
 	getURL := ""
 	if s.mapNext == "" && s.mapPrev == "" {
 		getURL = "https://pokeapi.co/api/v2/location-area/"
 	} else {
 		getURL = s.mapNext
+	}
+	res, err := http.Get(getURL)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		return err
+	}
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	b := bodyResults{}
+	err = json.Unmarshal(body, &b)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	for i := range b.Results {
+		fmt.Println(b.Results[i].Name)
+	}
+	s.mapNext = b.Next
+	s.mapPrev = b.Previous
+	return nil
+}
+
+func commandMapb(s *config) error {
+	getURL := ""
+	if s.mapPrev == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	} else {
+		getURL = s.mapPrev
 	}
 	res, err := http.Get(getURL)
 	if err != nil {
